e9933e509efaf16c3dcc73c0f814ae49a8bc4626
Vladimir Ortiz <vladimir.ortiz8015@alumnos.udg.mx>
2024-11-04 14:49:05 -0600

    Cambio en el mensaje de Carga inicial

---
+++ app/src/main/java/ortiz/vladimir/simulacion/MainActivity.kt
@@ -0,0 +1,47 @@
+package ortiz.vladimir.simulacion
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ortiz.vladimir.simulacion.ui.theme.SimulacionTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SimulacionTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Vladimir",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SimulacionTheme {
+        Greeting("Android")
+    }
+}
